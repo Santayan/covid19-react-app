02106cc3fe05b5068c4ec66aa95900fe5c14273e
Santayan Paul <santayan.paul@gmail.com>
2020-08-01 10:55:02 -0400

    Initial commit

---
+++ build/precache-manifest.55b4cee1f3c75c74baf1d5a0f4cd9bdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67afb0a6248560978356e8a84c016b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eef0d4ebaf7353c29e4f",
+    "url": "/static/css/main.2a38fb88.chunk.css"
+  },
+  {
+    "revision": "42e5f3b63bb4f70b7da8",
+    "url": "/static/js/2.e21b00a0.chunk.js"
+  },
+  {
+    "revision": "14ed076e8c9c3d038ba497650386b32b",
+    "url": "/static/js/2.e21b00a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef0d4ebaf7353c29e4f",
+    "url": "/static/js/main.fd7357f2.chunk.js"
+  },
+  {
+    "revision": "b8926f34e5132f2e2a62",
+    "url": "/static/js/runtime-main.a18345d7.js"
+  }
+]);
